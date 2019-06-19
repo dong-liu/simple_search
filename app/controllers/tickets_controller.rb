37d752f74dd2d59d96b8aa4746eaac3c8216524c
Dong Liu <dongliu.us@gmail.com>
2019-06-19 04:13:43 -0700

    add search functionality for tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -10,4 +10,21 @@ class TicketsController < ApplicationController
   	@ticket = Ticket.find(params[:id])
   end
 
+  def search
+  	if params[:field].nil?
+  		flash.now[:notice] = "Not a valid field"
+  	else
+  		@ticket = Ticket.find_by(params[:field] => params[:value]);
+  		if @ticket.nil?
+  			flash[:notice] = "Search tickets for #{params[:field]} with a value of #{params[:value]} - no results found"
+  		else
+  			@organization = Organization.find(@ticket.organization_id)
+  			@submitter = User.find(@ticket.submitter_id)
+  			@assignee = User.find(@ticket.assignee_id)
+  		end
+  	end
+  end
+  
+
+
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,20 +6,20 @@ class Ticket < ApplicationRecord
 	self.primary_key = :_id
 
 	belongs_to :organization
-	has_and_belongs_to_many :users
+	belongs_to :users
 
-  scope :shared_tickets, lambda { where(:shared_tickets => true) }
-  scope :incident_tickets, lambda { where(:has_incidents => true) }
-  scope :not_incident_tickets, lambda { where(:has_incidents => false) }
+  scope :shared_tickets, -> { where(:shared_tickets => true) }
+  scope :incident_tickets, -> { where(:has_incidents => true) }
+  scope :not_incident_tickets, -> { where(:has_incidents => false) }
 
-  scope :sorted, lambda { order("_id ASC") }
-  scope :sorted_by_type, lambda { order("type ASC") }
-  scope :sorted_by_priority, lambda { order("priority ASC") }
-  scope :sorted_by_status, lambda { order("status ASC") }
-  scope :sorted_by_origin, lambda { order("via ASC") }
-  scope :newest_first, lambda { order("created_at DESC") }
-  scope :due_at, lambda { order("due_at DESC") }
+  scope :sorted, -> { order("_id ASC") }
+  scope :sorted_by_type, -> { order("type ASC") }
+  scope :sorted_by_priority, -> { order("priority ASC") }
+  scope :sorted_by_status, -> { order("status ASC") }
+  scope :sorted_by_origin, -> { order("via ASC") }
+  scope :newest_first, -> { order("created_at DESC") }
+  scope :due_at, -> { order("due_at DESC") }
 
-  scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"]) }
+  scope :search_by_submitter_id, -> (id) { where(:submitter_id => id) }
 
 end

50819a3fa731a438b567f5d3254e4b25c3f5d794
Dong Liu <dongliu.us@gmail.com>
2019-06-19 04:16:28 -0700

    add the search functionality for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,22 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-  	@organization_id = "TBD"
+  	@organization_id = @user.organization_id
+  end
+
+  def search
+  	if params[:field].nil?
+  		flash.now[:notice] = "Not a valid field"
+  	else
+  		@user = User.find_by(params[:field] => params[:value]);
+  		if @user.nil?
+  			flash[:notice] = "Search users for #{params[:field]} with a value of #{params[:value]} - no results found"
+  		else
+	  		@organization = Organization.find(@user.organization_id)
+	  		#@tickets = Ticket.where(:submitter_id => @user._id)
+	  		@tickets = Ticket.search_by_submitter_id(@user.id)
+	  	end
+  	end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,21 @@ class User < ApplicationRecord
 	self.primary_key = :_id
 	 
 	belongs_to :organization
-	has_and_belongs_to_many :tickets
+	has_many :tickets
 
-	scope :active, lambda { where(:active => true) }
-	scope :inactive, lambda { where(:visible => false) }
-	scope :verified, lambda { where(:verified => true) }
-	scope :unverified, lambda { where(:verified => false) }
-	scope :shared, lambda { where(:shared => true) }
-	scope :notshared, lambda { where(:shared => false) }
-	scope :suspended, lambda { where(:suspended => true) }
-	scope :reinstated, lambda { where(:suspended => false) }
+	scope :active, -> { where(:active => true) }
+	scope :inactive, -> { where(:visible => false) }
+	scope :verified, -> { where(:verified => true) }
+	scope :unverified, -> { where(:verified => false) }
+	scope :shared, -> { where(:shared => true) }
+	scope :notshared, -> { where(:shared => false) }
+	scope :suspended, -> { where(:suspended => true) }
+	scope :reinstated, -> { where(:suspended => false) }
 
-  scope :sorted, lambda { order("_id ASC") }
-  scope :sorted_by_role, lambda { order("role ASC") }
-  scope :newest_first, lambda { order("created_at DESC") }
-  scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"]) }
+  scope :sorted, -> { order("_id ASC") }
+  scope :sorted_by_role, -> { order("role ASC") }
+  scope :newest_first, -> { order("created_at DESC") }
+  
+  #scope :search, -> {|query| where(["name LIKE ?", "%#{query}%"]) }
 
 end
